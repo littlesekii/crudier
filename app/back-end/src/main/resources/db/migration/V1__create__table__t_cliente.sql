7bf67c0e0f9b5937f16ca5ba5f364f5e0a074d50
littlesekii <contact.sekii@gmail.com>
2025-04-10 00:45:39 -0300

    fix.: corrigida a validação da integridade dos valores inseridos no banco

--- app/back-end/src/main/resources/db/migration/V1__create__table__t_cliente.sql
+++ app/back-end/src/main/resources/db/migration/V1__create__table__t_cliente.sql
@@ -1,10 +1,10 @@
 create table t_Cliente (
     id bigserial primary key,
-    nome varchar(255),
-    cpf varchar(11),
-    email varchar(255),
-    telefone varchar(20),
-    estado varchar(100),
-    cidade varchar(100),
-    endereco varchar (255)
+    nome varchar(255) not null,
+    cpf varchar(11) not null unique,
+    email varchar(255) null,
+    telefone varchar(20) not null,
+    estado varchar(100) not null,
+    cidade varchar(100) not null,
+    endereco varchar (255) not null
 )
